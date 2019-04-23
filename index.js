ad8bfc073c7e2a7544001d0494f745925498f2d7
Christian Bundy <christianbundy@fraction.io>
2019-04-23 13:28:20 -0700

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pullCat = require('pull-cat')
 const ssbRef = require('ssb-ref')
 const pullCatch = require('pull-catch')
 const pullTee = require('pull-tee')
+const debug = require('debug')('ssb-content-stream')
 
 const MB = 1024 * 1024
 const MAX_SIZE = 5 * MB
@@ -34,20 +35,24 @@ exports.init = (ssb) => {
               blobs.push(val)
             }
           })
+          debug('referenced blobs: %O', blobs)
 
           return blobs
         }),
         pull.flatten(), // items of arrays of strings => items of strings
         pull.unique(), // don't get the same blob twice
-        pull.map((hash) =>
-          pull(
+        pull.map((hash) => {
+          debug('getting blob: %s', hash)
+          return pull(
             ssb.blobs.get({
               hash,
               max: opts.max || MAX_SIZE
             }),
-            pullCatch() // disregard blobs that don't exist or are too big
+            pullCatch((err) => {
+              debug('harmless error pulling blob: %O', err)
+            }) // disregard blobs that don't exist or are too big
           )
-        ),
+        }),
         pull.flatten() // convert from pull-stream source to items
       ),
       ssb.createHistoryStream(opts)
@@ -78,14 +83,49 @@ exports.init = (ssb) => {
             })
           )
         ),
-        pull.filterNot(Buffer.isBuffer)
+        pull.filterNot(Buffer.isBuffer),
       )
     },
-    createBlobHandlerSource: (opts, cb) =>
-      pull(
-        contentStream.createSource(opts),
-        contentStream.createBlobHandler(cb)
-      )
+    createBlobHandlerSource: (opts, cb) => pull(
+      contentStream.createSource(opts),
+      contentStream.createBlobHandler(cb)
+    ),
+    createSink: (opts, cb) => {
+      // In a perfect world this would probably look like:
+      //
+      // 1. Receive all messages
+      // 2. Extract blob references
+      // 3. Save each message to database to validate
+      // 4. Receive all blobs
+      // 5. Hash each blob
+      // 6. Compare against extracted blob references and save blobs we want
+      //
+      // Unfortunately in the case of off-chain content we don't want to be in
+      // a position where we're saving a message before we have the blob in the
+      // blob store. This means that we need to save the blobs before we validate
+      // the message, which leads us to something that looks more like:
+      //
+      // 1. Receive all messages
+      // 2. Extract blob references
+      // 3. Receive all blobs
+      // 4. Hash each blob
+      // 5. Compare against extracted blob references to save blobs we want
+      // 6. Start listening for database errors
+      // 7. Save each message to database to validate
+      // 8. If a message is invalid, delete the uniquely referenced blob
+
+      const extractedBlobs = {
+        // '&a': [ '%b', '%c']
+      }
+
+      ssb.on('invalid', (err, msg) => {
+        // TODO test to make sure this actually emits on invalid messages
+        console.log(msg.key, err)
+      })
+
+      //      return ssb.createWriteStream((err) => 
+      // )
+    }
   }
 
   return contentStream
--- test.js
+++ test.js
@@ -2,23 +2,47 @@ const ssbServer = require('ssb-server')
 const ssbConfig = require('ssb-config')
 const pull = require('pull-stream')
 const test = require('tape')
+const { tmpdir } = require('os')
+const ssbKeys = require('ssb-keys')
 
 ssbServer
   .use(require('ssb-server/plugins/master'))
   .use(require('ssb-blobs'))
+  .use(require('ssb-blob-content'))
   .use(require('./'))
 
-const ssb = ssbServer(ssbConfig)
-
-const streamOpts = Object.assign(
+const config = Object.assign(
   {},
-  ssb.whoami(),
-  { limit: 10 }
+  ssbConfig,
+  {
+    path: tmpdir(Math.random()),
+    port: 40090,
+    keys: ssbKeys.generate()
+  }
 )
+
+const ssb = ssbServer(config)
+
+const streamOpts = Object.assign( {}, ssb.whoami())
 
 const collect = (t) => pull.collect((err, msgs) => {
   t.error(err, 'collect messages without error')
   t.equal(Array.isArray(msgs), true, 'messages is an array')
+})
+
+test('setup', (t) => {
+  t.plan(3)
+
+  ssb.blobContent.publish({ type: 'repeat', content: 'hello' }, (err, msg) => {
+    t.error(err)
+    ssb.blobContent.publish({ type: 'unique', content: 'world' }, (err, msg) => {
+      t.error(err)
+      ssb.blobContent.publish({ type: 'repeat', content: 'hello' }, (err, msg) => {
+        t.error(err)
+        t.end()
+      })
+    })
+  })
 })
 
 test('createSource(opts) + createBlobHandler()', (t) => {
@@ -37,8 +61,9 @@ test('createSource(opts) + createBlobHandler(cb)', (t) => {
   pull(
     ssb.contentStream.createSource(streamOpts),
     ssb.contentStream.createBlobHandler((err, blobNum) => {
+      console.log(blobNum)
       t.error(err, 'successful blob handler')
-      t.equal(typeof blobNum, 'number')
+      t.equal(blobNum, 2)
     }),
     collect(t)
   )
@@ -59,7 +84,7 @@ test('createBlobHandlerSource(opts)', (t) => {
   pull(
     ssb.contentStream.createBlobHandlerSource(streamOpts, (err, blobNum) => {
       t.error(err, 'successful blob handler')
-      t.equal(typeof blobNum, 'number')
+      t.equal(blobNum, 2)
     }),
     collect(t)
   )
